318592d15153fe90c37d08b50a65d4f0b4fb6bee
Marcin Kopa <marcin.info@gmail.com>
2018-12-02 12:40:27 +0100

    Add apidoc and .eslintignore file

--- src/router/src/ping.js
+++ src/router/src/ping.js
@@ -3,6 +3,18 @@ const { pingController } = require('../../controllers');
 
 const router = new Router();
 
+/**
+ * @api {get} /api/ping Request pong message
+ * @apiName Ping
+ * @apiGroup Test
+ *
+ * @apiSuccessExample {json} Success
+ *   HTTP/1.1 200 OK
+ *   {
+ *     "success": true,
+ *     "message": "pong"
+ *   }
+ */
 router.route('/ping')
   .get((...args) => pingController.get(...args));
 
